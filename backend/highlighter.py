af154799b04194cf2cd41536a806037395ae49df
hitanshu-p <hitanshu88@outlook.com>
2025-05-19 16:14:15 -0700

    installed gh-pages dependency

--- backend/highlighter.py
+++ backend/highlighter.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # Middleware to allow backend to receive requests from frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
