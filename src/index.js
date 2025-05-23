d637b81e79570d30a5dda108584c12dfd52ffaa3
Elsasc <coleelsasser911@gmail.com>
2025-05-22 22:43:58 -0700

    Fixing dependency issue, run audit fix --force to undo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.js";
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+    </Router>
   </React.StrictMode>
 );
